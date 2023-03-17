96ba9e331ba3c669d3cb2a162690313c256519d1
Yael Méndez <yael.mendez@inflexionsoftware.com>
2023-03-16 21:09:48 -0600

    Diseño de los input
    Creando diseño de la pantalla del login y los inputs para datos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { LoginScreenComponent } from './screens/login-screen/login-screen.compon
     BrowserAnimationsModule,
     NgxMaskModule.forRoot(options),
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/screens/login-screen/login-screen.component.ts
+++ src/app/screens/login-screen/login-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-login-screen',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-screen.component.scss']
 })
 export class LoginScreenComponent implements OnInit {
+  public type: string = 'password';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public showPassword(){
+    if(this.type == "password"){
+      $("#show-password").addClass("show-password");
+      $("#show-password").attr("data-password", true);
+      this.type = "text";
+    }else if(this.type == "text"){
+      $("#show-password").removeClass("show-password");
+      $("#show-password").attr("data-password", false);
+      this.type = "password";
+    }
   }
 
 }
