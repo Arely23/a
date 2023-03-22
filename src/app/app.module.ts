1463d4394aabd0d1f1461a5167a41ede32effb77
Yael Méndez <yael.mendez@inflexionsoftware.com>
2023-03-21 21:12:03 -0600

    Iniciando Registro-Screen
    En el componente de registro, se inicia el diseño, se agregan mat-inputs y los botones.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
 //Angular material
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+//Componentes
 import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
 import { RegistroUserScreenComponent } from './screens/registro-user-screen/registro-user-screen.component'
 
@@ -28,7 +30,8 @@ import { RegistroUserScreenComponent } from './screens/registro-user-screen/regi
     NgxMaskModule.forRoot(options),
     MatButtonModule,
     MatIconModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
